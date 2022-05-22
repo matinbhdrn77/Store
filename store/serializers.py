042717eb2a1d88ef806cd75e517858216ccc4640
matin bahadoran <matinbhdrn77@gmail.com>
2022-05-22 11:31:21 +0430

    update order serializer

--- store/serializers.py
+++ store/serializers.py
@@ -1,7 +1,6 @@
-from logging import exception
-import re
-from rest_framework import serializers
 from decimal import Decimal
+from django.db import transaction
+from rest_framework import serializers
 from .models import OrderItem, Product, Collection, Review, Cart, CartItem, Customer, Order
 
 
@@ -138,5 +137,19 @@ class CreateOrderSerializer(serializers.Serializer):
     cart_id = serializers.UUIDField()
 
     def save(self, **kwargs):
-        (customer, created) = Customer.objects.get_or_create(user_id=self.context['user_id'])
-        Order.objects.create(customer=customer)
+        with transaction.atomic():
+            cart_id = self.validated_data('cart_id')
+
+            (customer, created) = Customer.objects.get_or_create(user_id=self.context['user_id'])
+            order = Order.objects.create(customer=customer)
+
+            cart_items = CartItem.objects.select_related('product').filter(cart_id=cart_id)
+            order_items = [
+                OrderItem(
+                    order=order, product=item.product, 
+                    unit_price=item.product.unit_price, quantity=item.quantity
+                ) for item in cart_items
+            ]
+            OrderItem.objects.bulk_create(order_items)
+
+            Cart.objects.filter(pk=cart_id)
