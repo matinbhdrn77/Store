7ac5785a520481043e72590808922e27737247b2
matin bahadoran <matinbhdrn77@gmail.com>
2022-05-23 17:58:52 +0430

    initialize django cors headers

--- store/validators.py
+++ store/validators.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import ValidationError
 
 def validate_file_size(file):
-    max_file_size = 50
+    max_file_size = 500
     if file.size > max_file_size * 1024:
         raise ValidationError(f"The file size must be lower than {max_file_size}KB!")
