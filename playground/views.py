81ea3bbc40dc424229a12eeea9d22d919420ba9d
matin bahadoran <matinbhdrn77@gmail.com>
2022-05-24 18:45:17 +0430

    configure setting for email

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.core.mail import send_mail, mail_admins, BadHeaderError
 
 
 def say_hello(request):
+    try:
+        send_mail("subject", "messages", "info@mat.com", ["some@ex.com"])
+    except BadHeaderError:
+        pass
     return render(request, 'hello.html', {'name': 'Mosh'})
